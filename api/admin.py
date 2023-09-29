0ce1d64bca9e1fcada711941ca75066145206aba
Marwan Alkhatib <marwan.alkhatib.itedev@gmail.com>
2023-09-29 20:32:24 +0300

    Basic Setup

---
+++ api/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from .models import Meal, Rating
+
+# Register your models here.
+
+
+class MealAdmin(admin.ModelAdmin):
+    list_display = ["id", "title", "description"]
+    list_filter = ["title", "description"]
+    search_fields = ["title", "description"]
+
+
+class RatingAdmin(admin.ModelAdmin):
+    list_display = ["id", "meal", "user", "stars"]
+    # Cause the index_together in class Meta I Put index on each Combination to Enhance The Query ANd Filter
+    list_filter = ["meal", "user"]
+
+
+admin.site.register(Meal, MealAdmin)
+admin.site.register(Rating, RatingAdmin)

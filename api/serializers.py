7987923e25c7405b2e54336516bb72dbe742cfe5
Marwan Alkhatib <marwan.alkhatib.itedev@gmail.com>
2023-10-01 17:22:33 +0300

    Token User

--- api/serializers.py
+++ api/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers
 from .models import Meal, Rating
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
 
 
 class MealSerializer(serializers.ModelSerializer):
@@ -12,3 +14,14 @@ class RatingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Rating
         fields = ["id", "stars", "user", "meal"]
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "username", "password"]
+        extra_kwargs = {"password": {"write_only": True, "required": True}}
+
+    # def create(self, validated_data):
+    #     user = User.objects.create(**validated_data)
+    #     return user
--- api/views.py
+++ api/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
-from rest_framework import viewsets, status, request
+from rest_framework import viewsets, status
 from .models import Meal, Rating
-from .serializers import MealSerializer, RatingSerializer
+from .serializers import MealSerializer, RatingSerializer, UserSerializer
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from django.contrib.auth.models import User
-from django.core.exceptions import ObjectDoesNotExist
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.authtoken.models import Token
+from rest_framework.permissions import (
+    IsAuthenticated,
+    AllowAny,
+    IsAdminUser,
+    IsAuthenticatedOrReadOnly,
+)
 
 # Create your views here.
 
@@ -13,16 +20,23 @@ from django.core.exceptions import ObjectDoesNotExist
 class MealViewSet(viewsets.ModelViewSet):
     queryset = Meal.objects.all()
     serializer_class = MealSerializer
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
 
     @action(detail=True, methods=["POST"])
     def rate_meal(self, request, pk=None):
-        if "stars" in request.data and "username" in request.data:
-            meal = Meal.objects.get(id=pk)
-            stars = request.data["stars"]
-            username = request.data["username"]
-
+        if "stars" in request.data:
             try:
-                user = User.objects.get(username=username)
+                meal = Meal.objects.get(id=pk)
+            except Meal.DoesNotExist:
+                JsonResponse = {"response": f"Meal ID: [{pk}] Doesn't Exist"}
+                return Response(JsonResponse, status=status.HTTP_204_NO_CONTENT)
+            stars = request.data["stars"]
+            if not int(stars) in range(1, 6):
+                JsonResponse = {"message": "Stars Should Be Between 1 To 5"}
+                return Response(JsonResponse, status=status.HTTP_406_NOT_ACCEPTABLE)
+            else:
+                user = request.user
                 try:
                     rate = Rating.objects.get(user=user, meal=meal)
                     if rate.stars == int(stars):
@@ -47,10 +61,6 @@ class MealViewSet(viewsets.ModelViewSet):
                         "Data": rateSerializer.data,
                     }
                     return Response(JsonResponse, status=status.HTTP_201_CREATED)
-
-            except User.DoesNotExist:
-                JsonResponse = {"response": "User not found"}
-                return Response(JsonResponse, status=status.HTTP_400_BAD_REQUEST)
         else:
             JsonResponse = {"response": "Username Or Stars Not Provided"}
             return Response(JsonResponse, status=status.HTTP_400_BAD_REQUEST)
@@ -59,3 +69,40 @@ class MealViewSet(viewsets.ModelViewSet):
 class RatingViewSet(viewsets.ModelViewSet):
     queryset = Rating.objects.all()
     serializer_class = RatingSerializer
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def update(self, request, *args, **kwargs):
+        response = {"message": "This Is Not How You should Create\\Update Rating"}
+        return Response(response, status=status.HTTP_400_BAD_REQUEST)
+
+    def create(self, request, *args, **kwargs):
+        response = {"message": "This Is Not How You should Create\\Update Rating"}
+        return Response(response, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = (AllowAny,)
+
+    def get_permissions(self):
+        if self.action in ["list", "update", "destroy"]:
+            # Restrict "list," "update," and "destroy" actions to admin users only
+            self.permission_classes = [IsAdminUser]
+        return super().get_permissions()
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        user = serializer.save()  # Create the user and token
+
+        token = Token.objects.create(user=user)
+
+        # Customize the response to include the token key
+        response_data = {
+            "token": token.key,
+        }
+
+        return Response(response_data, status=status.HTTP_201_CREATED)
